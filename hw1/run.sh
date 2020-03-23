e0857b0e03f39ab4e3eb60c8754a83484bd8ccb8
Oren Zaharia <orenzah@gmail.com>
2020-03-24 00:11:42 +0200

    added if-else for transport protocol

--- hw1/run.sh
+++ hw1/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 . parse_yaml.sh
 eval $(parse_yaml config.yaml)
-RPID=$(ssh root@$REMOTE_IP "iperf3 -s -p $PORT > iperf3.txt & echo -e \$!")
-iperf3 -c $REMOTE_IP -p $PORT -t $TIME -i $INTERVAL --forceflush | tee $OUTPUT_FILE
+if [[ "$TRANSPORT" == "UDP" ]]; then
+	RPID=$(ssh root@$REMOTE_IP "iperf3 -s -u -p $PORT > iperf3.txt & echo -e \$!")
+	iperf3 -u -c $REMOTE_IP -p $PORT -t $TIME -i $INTERVAL --forceflush | tee $OUTPUT_FILE
+else
+	RPID=$(ssh root@$REMOTE_IP "iperf3 -s -p $PORT > iperf3.txt & echo -e \$!")
+	iperf3 -c $REMOTE_IP -p $PORT -t $TIME -i $INTERVAL --forceflush | tee $OUTPUT_FILE
+
+fi
 ssh root@$REMOTE_IP "kill -9 $RPID"
 
 
