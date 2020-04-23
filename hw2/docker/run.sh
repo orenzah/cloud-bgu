51330191033d40fc8512a16051d7d9f652563581
Oren Zaharia <orenzah@gmail.com>
2020-04-24 02:16:28 +0300

    change paths

---
+++ hw2/docker/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+. parse_yaml.sh
+eval $(parse_yaml config.yaml)
+RPID=$(ssh root@$REMOTE_IP "iperf3 -s -p $PORT >  /dev/null 2>&1  & echo -e \$!")
+if [[ "$TRANSPORT" == "UDP" ]]; then
+	iperf3 -u -c $REMOTE_IP -p $PORT -t $TIME -i $INTERVAL --forceflush | tee $OUTPUT_FILE
+else
+	iperf3 -c $REMOTE_IP -p $PORT -t $TIME -i $INTERVAL --forceflush | tee $OUTPUT_FILE
+
+fi
+ssh root@$REMOTE_IP "kill -9 $RPID"
