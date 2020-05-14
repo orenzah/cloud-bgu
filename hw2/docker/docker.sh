d80b85416aa023e0cba43efc53876bb4bd467774
Oren Zaharia <orenzah@gmail.com>
2020-05-14 18:08:26 +0300

    change user to root, and added missing files

--- hw2/docker/docker.sh
+++ hw2/docker/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . parse_yaml.sh
 eval $(parse_yaml config.yaml)
-CNT_ID=$(ssh orenzah@$REMOTE_IP "docker run --rm --name 204034482_203523006 -d -p $PORT:5201/tcp -p $PORT:5201/udp orenzah/bgu-cloud-iperf:latest")
+CNT_ID=$(ssh root@$REMOTE_IP "docker run --rm --name 204034482_203523006 -d -p $PORT:5201/tcp -p $PORT:5201/udp orenzah/bgu-cloud-iperf:latest")
 if [[ "$TRANSPORT" == "UDP" ]]; then
 
 	iperf3 -u -c $REMOTE_IP -p $PORT -t $TIME -i $INTERVAL --forceflush | tee $OUTPUT_FILE
@@ -12,5 +12,5 @@ fi
 #ssh orenzah@$REMOTE_IP "docker container stop $RPID"
 #B=$(ssh orenzah@$REMOTE_IP "docker exec $CNT_ID pidof iperf3")
 #A=$(ssh orenzah@$REMOTE_IP "docker exec $CNT_ID kill -9 $B")
-A=$(ssh orenzah@$REMOTE_IP "docker container stop $CNT_ID")
+A=$(ssh root@$REMOTE_IP "docker container stop $CNT_ID")
 echo "Script Done"
