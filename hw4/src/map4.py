b33c215f50fbf4b5b0b51848a4eadda91c0ecdf9
Oren Zaharia <orenzah@gmail.com>
2020-06-21 23:59:06 +0300

    fixed the complement

--- hw4/src/map4.py
+++ hw4/src/map4.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
-def comp(s, c):
+def comp1(s, c):
     l = list(s);
     for i in range(len(l)):
         l[i] = '{0}'.format(c - int(l[i]))
     return ''.join(l) + str(5 - len(l))
+
+def comp(s):
+	le1 = len(s);
+	s = s.ljust(5, '0')
+	l = list(s);
+	#l.reverse()
+	for i in range(len(l)):
+		l[i] = str(9 - int(l[i]))
+	return ''.join(l) + str(6 - le1)
+
 import sys
 for line in sys.stdin:
 	line = line.strip()
@@ -13,12 +23,12 @@ for line in sys.stdin:
 	bit1 = words[1].strip().replace('(' , '').replace(')' , '').strip()
 	ppid2 = words[2].strip().replace('(' , '').replace(')' , '')
 	bit2 = words[3].strip().replace('(' , '').replace(')' , '').strip()
-	ppid1 = comp(ppid1, 9)
-	bit1 = comp(bit1, 1)
-	ppid2 = comp(ppid2, 9)
-	bit2 = comp(bit2,1)
-	print(ppid1.rjust(5,ppid1[0]), end='')
+	ppid1 = comp(ppid1)
+	bit1 = comp1(bit1, 1)
+	ppid2 = comp(ppid2)
+	bit2 = comp1(bit2,1)
+	print(ppid1, end='')
 	print(bit1, end='_')
-	print(ppid2.rjust(5,ppid2[0]), end='')
+	print(ppid2, end='')
 	print(bit2, end=',')
 	print(line[-3::-1][::-1].replace('(' ,'').replace(')',''), flush=True)
